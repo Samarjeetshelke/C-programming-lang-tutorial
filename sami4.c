6c4b93be08434397b41cef711a8f6cd98c623df0
samual-official <59022780+samual-official@users.noreply.github.com>
2019-12-20 22:59:14 +0530

    correcting indentation

--- sami4.c
+++ sami4.c
@@ -4,28 +4,28 @@ void main(){
 	char g;
 	printf("please enter your gender,years of surves,qualification\n\nif qua.is graduat enter (0),if qua. is postgraduat enter (1)");
 	scanf("%c%d%d",&g,&yos,&qua);
-	if(g=='m' &&yos>=10&&qua==1){
+	if(g=='m' && yos>=10 && qua==1){
 		printf("your  salary is =RS15000");
 	}
-	else if(g=='m'&&yos>=10&&qua==0){
+	else if(g=='m' && yos>=10 && qua==0){
 		printf("your salary is =RS10000");
 	}
-	else if(g =='m'&&yos<10&&qua==1){
+	else if(g =='m' && yos<10 && qua==1){
 		printf("your salary is =RS10000");
 	}
-       else if(g =='m'&&yos>10&&qua==0){
+       else if(g =='m' && yos>10 && qua==0){
        printf("your salary is=RS7000");
 	}
-       else if(g=='f'&&yos>=10&&qua==1){
+       else if(g=='f' && yos>=10 && qua==1){
 	       printf("your salary is =RS12000");
        }
-       else if(g=='f'&&yos>=10&&qua==0){
+       else if(g=='f' && yos>=10 && qua==0){
 	       printf("your salary is =RS10000");
        }
-       else if(g=='f'&&yos<10&&qua==1){
+       else if(g=='f' && yos<10 && qua==1){
 	       printf("your salary is=RS10000");
        }
-       else if(g=='f'&&yos<10&&qua==0){
+       else if(g=='f' && yos<10 && qua==0){
 	       printf("your salary is=RS7000");
        }
 	getch();
