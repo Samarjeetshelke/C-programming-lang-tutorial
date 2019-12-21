a3a805ff08901479cf521791782f2ffb0403e5cd
samual-official <59022780+samual-official@users.noreply.github.com>
2019-12-21 17:41:30 +0530

    #

--- sami6looping(for).c
+++ sami6looping(for).c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+void main(){
+       int p,n,count;
+       float r,si;
+       for(count=1;count<=3;count=count+1){
+	printf("please enter principle valu,year,rate");
+	scanf("%d%d%f",&p,&n,&r);
+	si=p*n*r/100;
+	printf("simple intrest=RS%f",si);
+       }
 
+       return;
+
+}
